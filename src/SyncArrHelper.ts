55525c20637b580e61e5b06e434c8e515031bf1c
aleksandr <kondaurov.dev@gmail.com>
2017-08-04 12:49:56 +0300

    async run

--- src/SyncArrHelper.ts
+++ src/SyncArrHelper.ts
@@ -1,56 +1,62 @@
-export function arrNumbers2ArrayTuples(numbers: Array<number>): Array<[number, number]> {
+export function arrNumbers2ArrayTuples(numbers: Array<number>): Promise<Array<[number, number]>> {
 
-    let result: Array<[number, number]> = [];
+    return new Promise((resolve, reject) => {
 
-    let currentTuple: [number, number] = [1, 1];
+        let result: Array<[number, number]> = [];
 
-    let size = numbers.length;
+        let currentTuple: [number, number] = [1, 1];
 
-    console.log("numbers: " + numbers);
+        let size = numbers.length;
 
-    for (let i = 1; i < size; i++) {
+        for (let i = 1; i < size; i++) {
 
-        let isLast = i == size - 1;
+            let isLast = i == size - 1;
 
-        let isChanged = numbers[i] - numbers[i - 1] != 1;
+            let isChanged = numbers[i] - numbers[i - 1] != 1;
 
-        if (isLast) {
+            if (isLast) {
 
-            if (isChanged) {
-                result.push(currentTuple);
-                result.push([numbers[i], numbers[i]])
+                if (isChanged) {
+                    result.push(currentTuple);
+                    result.push([numbers[i], numbers[i]])
+                } else {
+                    currentTuple[1] = numbers[i];
+                    result.push(currentTuple)
+                }
+
             } else {
-                currentTuple[1] = numbers[i];
-                result.push(currentTuple)
-            }
 
-        } else {
+                if (isChanged) {
+                    result.push(currentTuple);
+                    currentTuple = [numbers[i], numbers[i]]
+                } else {
+                    currentTuple[1] = numbers[i]
+                }
 
-            if (isChanged) {
-                result.push(currentTuple);
-                currentTuple = [numbers[i], numbers[i]]
-            } else {
-                currentTuple[1] = numbers[i]
             }
 
         }
 
-    }
+        resolve(result)
 
-    return result
+    })
+
+
 
 }
 
-export function arrNumbersToStr(numbers: Array<number>): string {
+export function arrNumbersToStr(numbers: Array<number>): Promise<string> {
 
     return arrNumbers2ArrayTuples(numbers)
-        .map(t => {
-            if (t[0] == t[1]) {
-                return `${t[0]}`
-            } else {
-                return `${t[0]}-${t[1]}`
-            }
-        }).join(",");
+        .then(tuples => {
+            return tuples.map(t => {
+                if (t[0] == t[1]) {
+                    return `${t[0]}`
+                } else {
+                    return `${t[0]}-${t[1]}`
+                }
+            }).join(",");
+        })
 
 }
 
--- test/src/SyncArrHelper.ts
+++ test/src/SyncArrHelper.ts
@@ -9,13 +9,11 @@ describe("arrNumbers2ArrayTuples should work", () => {
 
         let numbers = [1,2,3,4,5,6,7,8];
 
-        let actual = Mod.arrNumbers2ArrayTuples(numbers);
-
-        expect(actual).to.eql([
-            [1, 8]
-        ]);
-
-        expect(Mod.arrNumbersToStr(numbers)).to.eql("1-8")
+        return Mod.arrNumbers2ArrayTuples(numbers).then((actual) => {
+            expect(actual).to.eql([
+                [1, 8]
+            ]);
+        });
 
     });
 
@@ -23,12 +21,12 @@ describe("arrNumbers2ArrayTuples should work", () => {
 
         let numbers = [1,3,4,5,6,7,8];
 
-        expect(Mod.arrNumbers2ArrayTuples(numbers)).to.eql([
-            [1, 1],
-            [3, 8]
-        ]);
-
-        expect(Mod.arrNumbersToStr(numbers)).to.eql("1,3-8")
+        return Mod.arrNumbers2ArrayTuples(numbers).then((actual) => {
+            expect(actual).to.eql([
+                [1, 1],
+                [3, 8]
+            ]);
+        })
 
     });
 
@@ -36,14 +34,14 @@ describe("arrNumbers2ArrayTuples should work", () => {
 
         let numbers = [1,2,3,4,5,6,100,1091,1999,2000,2001,2002];
 
-        expect(Mod.arrNumbers2ArrayTuples(numbers)).to.eql([
-            [1, 6],
-            [100, 100],
-            [1091, 1091],
-            [1999, 2002]
-        ]);
-
-        expect(Mod.arrNumbersToStr(numbers)).to.eql("1-6,100,1091,1999-2002")
+        Mod.arrNumbers2ArrayTuples(numbers).then((actual) => {
+            expect(actual).to.eql([
+                [1, 6],
+                [100, 100],
+                [1091, 1091],
+                [1999, 2002]
+            ]);
+        });
 
     });
 
@@ -51,15 +49,60 @@ describe("arrNumbers2ArrayTuples should work", () => {
 
         let numbers = [1,2,3,15,23];
 
-        expect(Mod.arrNumbers2ArrayTuples(numbers)).to.eql([
-            [1, 3],
-            [15, 15],
-            [23, 23]
-        ]);
-
-        expect(Mod.arrNumbersToStr(numbers)).to.eql("1-3,15,23")
+        Mod.arrNumbers2ArrayTuples(numbers).then((actual) => {
+            expect(actual).to.eql([
+                [1, 3],
+                [15, 15],
+                [23, 23]
+            ]);
+        })
 
     })
 
 });
+
+describe("arrNumbers2Str should work", () => {
+
+    it("case 1", () => {
+
+        let numbers = [1,2,3,4,5,6,7,8];
+
+        return Mod.arrNumbersToStr(numbers).then((actual) => {
+            expect(actual).to.eql("1-8");
+        });
+
+    });
+
+    it("case 2", () => {
+
+        let numbers = [1,3,4,5,6,7,8];
+
+        return Mod.arrNumbersToStr(numbers).then((actual) => {
+            expect(actual).to.eql("1,3-8");
+        })
+
+    });
+
+    it("case 3", () => {
+
+        let numbers = [1,2,3,4,5,6,100,1091,1999,2000,2001,2002];
+
+        Mod.arrNumbersToStr(numbers).then((actual) => {
+            expect(actual).to.eql("1-6,100,1091,1999-2002")
+        })
+
+    });
+
+    it("case 4", () => {
+
+        let numbers = [1,2,3,15,23];
+
+        Mod.arrNumbersToStr(numbers).then((actual) => {
+            expect(actual).to.eql("1-3,15,23")
+        })
+
+    })
+
+});
+
 
